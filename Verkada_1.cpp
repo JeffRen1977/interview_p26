cfea67e39166c4eb4ef278b1cfb479589f069170
JeffRen1977 <90067002+JeffRen1977@users.noreply.github.com>
2026-07-30 16:42:47 -0700

    Update workers class for template support and shutdown
    Refactor workers class to be template-based and improve thread safety with shutdown handling.

--- Verkada_1.cpp
+++ Verkada_1.cpp
@@ -7,28 +7,38 @@ Thread-safe push() and pop().
 Proper handling of condition variables to block push() when full, and block pop() when empty.
 Clean thread shutdown / cancellation handling.
 
+template <typname T>
 class workers
 {
    workers(int cap){
    capacity = cap;
+   shutdown = false;
    }
-void push(FrameBuffer frame_buffer){
+void push(T frame_buffer){
     std::unique_lock<std::mutext> lock(mu);
-    not_full.wait(lock,[this]{buffer.size()<capacity;});
+    not_full.wait(lock,[this]{buffer.size()<capacity||shutdown;});
     buffer.push(frame_buffer);
     not.empty.notify_one();
 }
-Frame pop(){
+T pop(){
     std::unique_lock<std::mutext> lock(mu);
-    not_emtpy.wait(lock,[this]{buffer.size()>0;});
-    Frame buf buffer.front();
+    not_emtpy.wait(lock,[this]{buffer.size()>0||shut_down;});
+    T buf=buffer.front();
     buffer.pop();
     not_full.notify_one();
+    return buf
+}
+void shutdown(){
+    std::unique_lock<std::mutext> lock(mu);
+    shut_down=true;
 }
 private:
-   queue<Frame> buffer;
-   std::condition_wait not_full;
-   std::condition_wait not empty;
+   queue<T> buffer;
+   std::condition_variable not_full;
+   std::condition_variaable not empty;
    int capapcity;
    std::mutex mu;
+   bool shutdown;
 }
+
+
